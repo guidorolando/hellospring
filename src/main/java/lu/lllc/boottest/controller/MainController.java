0dcb8a4c7ec4db9dde7f9d329c266ebac98c70fa
guidorolando91@gmail.com <Rolando91>
2019-05-14 11:14:55 -0400

    add controller

--- src/main/java/lu/lllc/boottest/controller/MainController.java
+++ src/main/java/lu/lllc/boottest/controller/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 	
-	@RequestMapping("/")
+	@RequestMapping("/controller")
 	public String hello(){
 		return "hello";
 	}
