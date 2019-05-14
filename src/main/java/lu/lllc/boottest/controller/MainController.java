9eefc1a85a196d2d81e5397cda76b0dcf16aa941
guidorolando91@gmail.com <Rolando91>
2019-05-14 11:48:08 -0400

    add controller

--- src/main/java/lu/lllc/boottest/controller/MainController.java
+++ src/main/java/lu/lllc/boottest/controller/MainController.java
@@ -1,7 +1,12 @@
 package lu.lllc.boottest.controller;
 
+import org.omg.CORBA.portable.ResponseHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import sun.security.provider.certpath.OCSPResponse;
 
 @Controller
 public class MainController {
@@ -9,5 +14,11 @@ public class MainController {
 	@RequestMapping("/controller")
 	public String hello(){
 		return "hello";
+	}
+
+	@RequestMapping(method = RequestMethod.POST, name = "/save")
+	public String save(Object object){
+
+		return "llego";
 	}
 }
